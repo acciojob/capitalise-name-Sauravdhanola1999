5596b8c66f1c6c1695dac02c99be59b9bf1274c8
Saurav Singh Dhanola <125140898+Sauravdhanola1999@users.noreply.github.com>
2024-11-15 15:01:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let fname = document.getElementById("fname")
+
+fname.addEventListener("blur", () => {
+	let temp = fname.value.toUpperCase();
+
+	fname.value = temp
+})
